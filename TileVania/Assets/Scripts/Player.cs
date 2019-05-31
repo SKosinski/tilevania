8c6517509bd1a89b13a39e504b83b7d38018a12f
SKosinski <stasiu.kosinski@hotmail.com>
2019-05-31 16:52:14 +0200

    After part 2, jumping included

--- TileVania/Assets/Scripts/Player.cs
+++ TileVania/Assets/Scripts/Player.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Config
+    [SerializeField] float runSpeed = 1f;
+    [SerializeField] float jumpSpeed = 1f;
+    //State
+    bool isAlive = true;
+
+    //Cached component references
+    Rigidbody2D myRigidBody;
+    Animator myAnimator;
+    Collider2D myCollider2D;
+
     void Start()
     {
-        
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myCollider2D = GetComponent<Collider2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Run();
+        if ( myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground")) )
+        {
+            Jump();
+        }
+    } 
+
+    void Run()
+    {
+        float controlThrow = Input.GetAxis("Horizontal");
+
+        if (controlThrow<0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+            myAnimator.SetBool("isRunning", true);
+        }
+        else if (controlThrow>0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+            myAnimator.SetBool("isRunning", true);
+        }
+        else
+        {
+            myAnimator.SetBool("isRunning", false);
+        }
+
+        Vector2 playerVelocity = new Vector2(runSpeed * controlThrow, myRigidBody.velocity.y);
+
+        print(playerVelocity);
+        myRigidBody.velocity = playerVelocity;
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocity = new Vector2(0, jumpSpeed);
+            myRigidBody.velocity += jumpVelocity;
+        }
     }
 }
